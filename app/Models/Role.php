04a262c26b1234138abde0cd56547b3dba8aafcb
Hiren Chhatbar <hc.rajkot@gmail.com>
2023-10-17 12:47:49 +0530

    docs(role): model

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -1,9 +1,22 @@
 <?php
+/**
+ * This file is part of the Igitt package.
+ *
+ * (c) Hiren Chhatbar <hc.rajkot@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace App\Models;
 
 use Hiren\Igitt\Models\Role as BaseRole;
 
+/**
+ * Role class
+ *
+ * @author Hiren Chhatbar <hc.rajkot@gmail.com>
+ */
 class Role extends BaseRole
 {
     /**
